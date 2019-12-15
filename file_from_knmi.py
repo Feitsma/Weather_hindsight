18cebd47091f96b9fb8ecf8567501a9b583166da
Feitsma <he.feitsma@gmail.com>
2019-12-15 18:35:18 +0100

    Nieuwe historische gegevensbestand gemaakt. Nu met knmi lib.

---
+++ file_from_knmi.py
@@ -0,0 +1,25 @@
+import wget
+import knmi
+import pandas as pd
+import datetime
+
+#url = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi/&stns=235:280:260&vars=VICL:PRCP&byear=1970&bmonth=1&bday=1&eyear=2009&emonth=8&eday=18"
+#wget.download(url)
+
+#datum = datetime.datetime.now()
+#dag = int(datum.strftime("%d"))
+#maand = int(datum.strftime("%m"))
+#jaar = int(datum.strftime("%Y"))
+#end = '{}{}{}'.format(jaar,maand,dag)
+
+
+df = knmi.get_day_data_dataframe(stations=[280])
+#print(df.disclaimer)
+#print(df.stations)
+#print(df.legend)
+#df = df.rename(columns=df.legend)
+print(df)
+variables = knmi.variables
+min_temp = df['TN'].iloc[-1]/10 #min. temp
+max_temp = df['TX'].iloc[-1]/10 #max. temp
+rain = df['RH'].iloc[-1]/10 #rain
--- historische_gegevens.py
+++ historische_gegevens.py
@@ -5,18 +5,16 @@ import datetime
 import mysql.connector
 
 
+
 datum = datetime.datetime.now()
 dag = int(datum.strftime("%d"))
-year = datum.strftime("%Y")
-month = datum.strftime("%m")
 dag_gisteren = dag-1
-dagdb = str(dag_gisteren)
 weerstatistieken = pd.read_html(io='https://weerstatistieken.nl/eelde', decimal=',', thousands='.')
 temperaturen = weerstatistieken[0] #pak de tabel met temperatuurdata
 vandaag = temperaturen.iloc[dag_gisteren] #vandaag is het de 14e, maar 13 ivm python indexing
 max = float(vandaag[2]) #max temperatuur vandaag
 min = float(vandaag [1])
-datumdb = (year, '-', month, "-", dagdb)
+datumdb = datetime.datetime.now() - datetime.timedelta(days=1) #Today we scrape the data of yesterday, one day needs to be substracted
 
 conn = mysql.connector.connect(user= dbsettings.user, password = dbsettings.password, database=dbsettings.database, host=dbsettings.host)
 cursor = conn.cursor()
