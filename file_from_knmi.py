30c7f5251bb6b36f7530f6989548308c4a7f267a
weetgeen <hans-pot@hotmail.com>
2019-12-16 13:41:45 +0100

    Only retrieve yesterdays info

--- file_from_knmi.py
+++ file_from_knmi.py
@@ -1,4 +1,4 @@
-import wget
+#import wget
 import knmi
 import pandas as pd
 import datetime
@@ -6,14 +6,17 @@ import datetime
 #url = "http://projects.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi/&stns=235:280:260&vars=VICL:PRCP&byear=1970&bmonth=1&bday=1&eyear=2009&emonth=8&eday=18"
 #wget.download(url)
 
-#datum = datetime.datetime.now()
+#Get the date of yesterday to retreive its information from KNMI
+yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+
 #dag = int(datum.strftime("%d"))
 #maand = int(datum.strftime("%m"))
 #jaar = int(datum.strftime("%Y"))
 #end = '{}{}{}'.format(jaar,maand,dag)
 
+#Station 280 is Eelde
+df = knmi.get_day_data_dataframe(start=yesterday,stations=[280])
 
-df = knmi.get_day_data_dataframe(stations=[280])
 #print(df.disclaimer)
 #print(df.stations)
 #print(df.legend)
