c40bb66ed1751f778fa8cb35874badaf1a2cc868
weetgeen <hans-pot@hotmail.com>
2019-12-16 09:36:27 +0100

    Changed DB column name to prediction_for

--- addDB.py
+++ addDB.py
@@ -7,9 +7,9 @@ def add(table,day,T_max,T_min,prec_prob,mm,wind,prediction_in_days):
     conn = mysql.connector.connect(user=dbsettings.user, password=dbsettings.password, database=dbsettings.database,
                                    host=dbsettings.host)
     cursor = conn.cursor()
-    add_positionlog = ("INSERT INTO " + table + " (DateTime,T_max,T_min,prec_prob,mm,wind,prediction_in_days) VALUES (%(DateTime)s, %(T_max)s, %(T_min)s, %(prec_prob)s,%(mm)s,%(wind)s,%(prediction_in_days)s)")
+    add_positionlog = ("INSERT INTO " + table + " (prediction_for,T_max,T_min,prec_prob,mm,wind,prediction_in_days) VALUES (%(prediction_for)s, %(T_max)s, %(T_min)s, %(prec_prob)s,%(mm)s,%(wind)s,%(prediction_in_days)s)")
     data_position = {
-    'DateTime': day,
+    'prediction_for': day,
     'T_max': T_max,
     'T_min': T_min,
     'prec_prob': prec_prob,
