442847611e6aa34081127106916168f657eb0ca7
Feitsma <he.feitsma@gmail.com>
2019-12-16 19:55:25 +0100

    Data_yesterday finished.

---
+++ data_yesterday.py
@@ -0,0 +1,21 @@
+import knmi
+import pandas as pd
+import wind_Converter
+import datetime
+import addDB
+
+#Get the date of yesterday to retreive its information from KNMI
+yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+#Station 280 is Eelde
+df = knmi.get_day_data_dataframe(stations=[280], start=yesterday)
+#variables = knmi.variables #handig als je wilt weten wat je allemaal kunt oproepen van KNMI
+#print(variables)
+min_temp = float(df['TN'].iloc[-1]/10) #min. temp tijdens laatste entry (moet gisteren zijn)
+max_temp = float(df['TX'].iloc[-1]/10) #max. temp
+rain = float(df['RH'].iloc[-1]/10) #rain
+wind_speed = df['FG'].iloc[-1]/10 #mean windspeed (m/s)
+Beaufort = wind_Converter.meteric_to_Beaufort(wind_speed)
+wind_direction = df['DDVEC'].iloc[-1] #wind direction (360=north, 90=east, 180=south, 270=west, 0=calm/variable)
+
+addDB.add_data_yesterday(yesterday, max_temp, min_temp, rain, Beaufort)
+
---
+++ wind_Converter.py
@@ -0,0 +1,29 @@
+def meteric_to_Beaufort(wind_speed):
+    if 0 <= wind_speed <= 0.2:
+        Beaufort = 0
+    elif 0.3 <= wind_speed <= 1.5:
+        Beaufort = 1
+    elif 1.6 <= wind_speed <= 3.3:
+        Beaufort = 2
+    elif 3.4 <= wind_speed <= 5.4:
+        Beaufort = 3
+    elif 5.5 <= wind_speed <= 7.9:
+        Beaufort = 4
+    elif 8 <= wind_speed <= 10.7:
+        Beaufort = 5
+    elif 10.8 <= wind_speed <= 13.8:
+        Beaufort = 6
+    elif 13.9 <= wind_speed <= 17.1:
+        Beaufort = 7
+    elif 17.2 <= wind_speed <= 20.7:
+        Beaufort = 8
+    elif 20.8 <= wind_speed <= 24.4:
+        Beaufort = 9
+    elif 24.5 <= wind_speed <= 28.4:
+        Beaufort = 10
+    elif 28.5 <= wind_speed <= 32.6:
+        Beaufort = 11
+    elif 32.7 <= wind_speed:
+        Beaufort = 12
+
+    return Beaufort
