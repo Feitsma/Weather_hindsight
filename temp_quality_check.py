9a7a3257f6b2b44ef23ad00792a03bb968c42e13
weetgeen <hans-pot@hotmail.com>
2020-01-05 12:45:37 +0100

    Create plot

--- temp_quality_check.py
+++ temp_quality_check.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import mysql.connector
 import dbsettings
+import matplotlib.pyplot as plt
 
 conn = mysql.connector.connect(user=dbsettings.user,
                                    password=dbsettings.password,
@@ -8,9 +9,12 @@ conn = mysql.connector.connect(user=dbsettings.user,
                                    host=dbsettings.host)
 
 
-temps_prediction = pd.read_sql("""SELECT AVG((prediction.T_max - t.T_max)) AS T_max_error, AVG((prediction.T_min - t.T_min)) AS T_min_error, 
+temps_prediction = pd.read_sql("""SELECT AVG((ABS(prediction.T_max - t.T_max))) AS T_max_error, AVG((ABS(prediction.T_min - t.T_min))) AS T_min_error, 
                                           prediction_in_days
                                           
                                   FROM prediction 
                                   INNER JOIN t on prediction.prediction_for = t.date 
                                   GROUP BY prediction_in_days""", conn)
+
+temps_prediction.plot(x='prediction_in_days', y=['T_max_error', 'T_min_error'])
+
