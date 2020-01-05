367d70df04f37d415b2773ceb396a989d3d6d2eb
Feitsma <he.feitsma@gmail.com>
2020-01-05 15:54:12 +0100

    Corrected amount of rain. -0.1 is now displayed as 0.01

--- data_yesterday.py
+++ data_yesterday.py
@@ -13,6 +13,8 @@ variables = knmi.variables #handig als je wilt weten wat je allemaal kunt oproep
 min_temp = float(df['TN'].iloc[-1]/10) #min. temp tijdens laatste entry (moet gisteren zijn)
 max_temp = float(df['TX'].iloc[-1]/10) #max. temp
 rain = float(df['RH'].iloc[-1]/10) #rain
+if rain == -0.1: #in order to show that it has raind but just a little bit.
+    rain = 0.01
 wind_speed = df['FG'].iloc[-1]/10 #mean windspeed (m/s)
 Beaufort = converter_functions.meteric_to_Beaufort(wind_speed)
 wind_direction = int(df['DDVEC'].iloc[-1]) #wind direction (360=north, 90=east, 180=south, 270=west, 0=calm/variable)
