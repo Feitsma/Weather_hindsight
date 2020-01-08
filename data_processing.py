2dee5034f3390937678a527b094fc778a2a9eadf
weetgeen <hans-pot@hotmail.com>
2020-01-08 23:05:03 +0100

    collect all functions in one file

--- data_processing.py
+++ data_processing.py
@@ -147,9 +147,63 @@ def plot_wind_outlook():
     plt.close()
     return plt
 
-#only run three functions, inside these functions, the others are requested.
+
+def plot_average_temp_error():
+    """Plots the average total error vs days of prediction, and saves the plot on webserver"""
+
+    conn = mysql.connector.connect(user=dbsettings.user,
+                                   password=dbsettings.password,
+                                   database=dbsettings.database,
+                                   host=dbsettings.host)
+
+    temps_prediction = pd.read_sql("""SELECT 
+                                        AVG((ABS(prediction.T_max - t.T_max))) AS T_max_error, 
+                                        AVG((ABS(prediction.T_min - t.T_min))) AS T_min_error, 
+                                        (prediction_in_days * -1) AS prediction_in_days
+    
+                                      FROM prediction 
+                                      INNER JOIN t on prediction.prediction_for = t.date 
+                                      GROUP BY prediction_in_days""", conn)
+
+    plt.ioff()
+    plot = temps_prediction.plot(x='prediction_in_days', y=['T_max_error', 'T_min_error'],
+                                 title='Average error in temperature outlook')
+    plot.set_xlabel('Outlook in days')
+    plot.set_ylabel('Temperature ($^\circ$C)')
+    plot = plot.get_figure()
+    # Save figure to webserver location
+    plot.savefig('/var/www/hp-iot/images/weather-hindsight/temp_average_outlook.png')
+
+
+def plot_average_mm_error()
+    """Plots the average total error vs days of prediction, and saves the plot on webserver"""
+
+    conn = mysql.connector.connect(user=dbsettings.user,
+                                   password=dbsettings.password,
+                                   database=dbsettings.database,
+                                   host=dbsettings.host)
+
+    temps_prediction = pd.read_sql("""SELECT 
+                                        AVG((ABS(prediction.mm - t.mm))) AS mm_error, 
+                                        (prediction_in_days * -1) AS prediction_in_days
+    
+                                      FROM prediction 
+                                      INNER JOIN t on prediction.prediction_for = t.date 
+                                      GROUP BY prediction_in_days""", conn)
+
+    plt.ioff()
+    plot = temps_prediction.plot(x='prediction_in_days', y='mm_error', title='Average error in precipitation outlook')
+    plot.set_xlabel('Outlook in days')
+    plot.set_ylabel('Precipitation (mm)')
+    plot = plot.get_figure()
+    #Save figure to webserver location
+    plot.savefig('/var/www/hp-iot/images/weather-hindsight/rain_average_outlook.png')
+
+
+
+
 plot_temp_outlook()
 plot_rain_outlook()
 plot_wind_outlook()
-#print(yesterday_rain())
-#print(rain_prediction())
+plot_average_temp_error()
+plot_average_mm_error()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ This file is run each day at 12:00 and
     - Scrapes the data from weerplaza (scrape_weerplaza.py)
     - Scrapes the actual weather of the day before (data_yesterday.py)
 
-    - Preform a quality check
+    - Preform a quality check of the data (data_processing.py)
 """
 
 
@@ -11,5 +11,3 @@ import scrape_weerplaza
 import data_yesterday
 
 import data_processing
-import temp_quality_check
-import mm_quality_check
